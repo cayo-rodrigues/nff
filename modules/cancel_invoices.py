49feb71d2f2fc1055782f248044303febaf142a0
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-04-08 17:02:25 -0300

    feat: check mandatory fields on invoices canceling sheet

--- modules/cancel_invoices.py
+++ modules/cancel_invoices.py
@@ -1,6 +1,20 @@
 from pandas import DataFrame
 
+from apis import GUI, DataBase, Logger
+from constants.db import MandatoryFields
+from utils import exceptions
+
 
 def cancel_invoices(entities: DataFrame, invoices_cancelings: DataFrame):
     print(":D")
-    ...
+
+    db = DataBase()
+    gui = GUI()
+
+    Logger.validating_db_fields()
+    try:
+        db.check_mandatory_fields(entities)
+        db.check_mandatory_fields(invoices_cancelings, MandatoryFields.INVOICE_CANCELING)
+    except (exceptions.MissingFieldsError, exceptions.EmptySheetError) as e:
+        gui.display_error_msg(msg=e.message)
+        exit()
