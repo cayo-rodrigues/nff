9d4f1b130393e2bb9d7ee1709fe8656fa2f82a52
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-06-04 21:23:35 -0300

    feat(desktop-app): enhance create invoices page
    - new feature of adding multiple invoices
    - new feature of opening dialog for (eventually) manage invoice items

--- desktop/scripts/invoices-page.js
+++ desktop/scripts/invoices-page.js
@@ -12,10 +12,40 @@ export function createInvoicesPage() {
     subEntry2.classList.remove(className)
 
     const contentCore = document.querySelector("#content__core")
-    contentCore.innerHTML = ""
     contentCore.innerHTML = `
         <form class="invoices-form">
-            <div class="invoices-form__inputs-container">
+            <div class="invoices-form__sections-container">
+                ${invoicesFormSection(1)}
+            </div>
+
+            <button class="invoices-form__button" type="submit">Emitir Notas Fiscais</button>
+            <button class="invoices-form__add-section-button" type="button">+</button>
+        </form>
+    `
+    
+    const sectionsContainer = document.querySelector('.invoices-form__sections-container')
+    sectionsContainer.addEventListener('click', ({ target }) => {
+        if (target.id && target.id.includes('open-dialog-button')) {
+            document.getElementById(`items-dialog-${target.dataset.invoiceId}`).showModal()
+        }
+        else if (target.id && target.id.includes('close-dialog-button')) {
+            document.getElementById(`items-dialog-${target.dataset.invoiceId}`).close()
+        }
+    })
+
+    const addSectionButton = contentCore.querySelector('.invoices-form__add-section-button')
+    addSectionButton.addEventListener('click', () => {
+        const invoiceId = sectionsContainer.childElementCount + 1
+        sectionsContainer.innerHTML += invoicesFormSection(invoiceId)
+    })
+}
+
+
+function invoicesFormSection(id) {
+    return `
+        <section class="invoices-form__section">
+            <h3>Nota Fiscal ${id}</h3>
+            <div id="${id}" class="invoices-form__inputs-container">
 
                 <div class="invoices-form__input">
                     <label for="sender-input">Remetente</label>
@@ -51,7 +81,7 @@ export function createInvoicesPage() {
                 </div>
                 <div class="invoices-form__input">
                     <label for="shipping-input">Frete</label>
-                    <input type="text" name="shipping" id="shipping-input">
+                    <input type="number" step=0.01 name="shipping" id="shipping-input">
                 </div>
                 <div class="invoices-form__input">
                     <label for="add_shipping_to_total_value-input">Adicionar Frete ao Total</label>
@@ -84,13 +114,95 @@ export function createInvoicesPage() {
                 </div>
                 <div class="invoices-form__input">
                     <label>Itens da Nota Fiscal</label>
-                    <button type="button" class="invoices-form__itens-modal-button">Gerenciar Itens</button>
+                    <button 
+                        type="button"
+                        id="open-dialog-button-${id}"
+                        class="invoices-form__items-dialog-button"
+                        data-invoice-id="${id}"
+                    >
+                        Gerenciar Itens
+                    </button>
+                </div>
+                
+                ${manageInvoiceItemsDialog(id)}
+
+            </div>
+        </section>
+    `
+}
+
+function manageInvoiceItemsDialog(id) {
+    return `
+        <dialog id="items-dialog-${id}" data-invoice-id="${id}" class="invoice-items-dialog">
+            <div class="invoice-items-dialog__heading">
+                <h3>Itens da Nota Fiscal ${id}</h3>
+                    
+                <div class="invoice-items-dialog__buttons-container">
+                    <button 
+                        type="button"
+                        class="invoice-items-dialog__button invoice-items-dialog__confirm-button"
+                    >
+                        Confirmar
+                    </button>
+                    <button
+                        type="button"
+                        class="invoice-items-dialog__button invoice-items-dialog__cancel-button"
+                        id="close-dialog-button-${id}"
+                        data-invoice-id="${id}"
+                    >
+                        Cancelar
+                    </button>
+                </div>
+            </div>
+
+            <hr/>
+            
+            <div class="invoices-form__inputs-container">
+                
+                <div class="invoices-form__input">
+                    <label for="group-input">Grupo</label>
+                    <select name="group" id="group-input">
+                        <option value="Gado bovino para corte">Gado bovino para corte</option>
+                    </select>
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="ncm-input">NCM</label>
+                    <input type="text" name="ncm" id="ncm-input">
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="description-input">Descrição</label>
+                    <input type="text" name="description" id="description-input">
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="origin-input">Origem</label>
+                    <select name="origin" id="origin-input">
+                        <option value="Nacional">Nacional</option>
+                    </select>
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="unity_of_measurement-input">Unidade de medida</label>
+                    <select name="unity_of_measurement" id="unity_of_measurement-input">
+                        <option value="CB">CB</option>
+                    </select>
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="quantity-input">Quantidade</label>
+                    <input type="number" step=0.01 name="quantity" id="quantity-input">
+                </div>
+
+                <div class="invoices-form__input">
+                    <label for="value_per_unity-input">Valor Unitário</label>
+                    <input type="number" step=0.01 name="value_per_unity" id="value_per_unity-input">
                 </div>
 
             </div>
 
-            <button class="invoices-form__button" type="submit">Emitir Notas Fiscais</button>
-        </form>
+        </dialog>
     `
 }
 
