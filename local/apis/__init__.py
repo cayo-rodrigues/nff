65bfcd81bccca2461b5bc664b5f6a6ff7d24bb8f
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-09-07 14:51:16 -0300

    clean up a little bit

--- local/apis/__init__.py
+++
@@ -1,6 +0,0 @@
-from .browser import Browser
-from .database import DataBase, NFFDataFrame
-from .file_manager import FileManager
-from .gui import GUI
-from .logger import Logger
-from .siare import Siare
--- local/constants/standards.py
+++
@@ -1,7 +0,0 @@
-PROJECT_NAME = "NFF"
-PROJECT_FULL_NAME = "NFF - Nota Fiscal Fácil"
-
-TRUTHY_STRS = ["sim", "sin", "si", "s", "1"]
-FALSY_STRS = ["não", "nao", "na", "n", "0"]
-
-STANDARD_SLEEP_TIME = 0.25
