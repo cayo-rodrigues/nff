f3b09bc01ade9d37e913a739bc5c33f1133ca493
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-10-12 13:01:36 -0300

    fix: add missing property

--- ss-api/constants/paths.py
+++ ss-api/constants/paths.py
@@ -16,6 +16,7 @@ class XPaths:
     LOGIN_IE_INPUT = "/html/body/div[5]/div[2]/div/div[2]/form/div/div/div/div[1]/div[1]/div[2]/div[2]/div[1]/input"
     LOGIN_CPF_INPUT = "/html/body/div[5]/div[2]/div/div[2]/form/div/div/div/div[1]/div[1]/div[3]/div/div[1]/input"
     LOGIN_PASSWORD_INPUT = "/html/body/div[5]/div[2]/div/div[2]/form/div/div/div/div[1]/div[1]/div[5]/div/div[1]/input"
+    LOGIN_ERROR_FEEDBACK = '//*[@id="mensagem"]'
 
     # require invoice page
     INVOICE_BASIC_DATA_OPERATION_SELECT_INPUT = "/html/body/div[3]/div[2]/div/div[3]/div/form/table[3]/tbody/tr[4]/td[2]/div/div[1]"
