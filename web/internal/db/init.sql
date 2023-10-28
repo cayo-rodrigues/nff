6cf23ee9c21a48e586ca757724749cc8a9e97590
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-10-28 17:04:04 -0300

    fix: some styling and other minor issues

--- web/internal/db/init.sql
+++ web/internal/db/init.sql
@@ -53,4 +53,4 @@ CREATE TABLE IF NOT EXISTS invoices_cancelings (
     req_status VARCHAR(7) DEFAULT 'pending', -- success, warning, error, pending
     req_msg VARCHAR(256) DEFAULT 'Em andamento...',
     CONSTRAINT fk_canceling_entity FOREIGN KEY (entity_id) REFERENCES entities(id) ON DELETE CASCADE
-)
+);
