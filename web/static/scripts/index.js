1920854be104e4d4686a3ed97926a55ba2842855
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-01-17 18:33:18 -0300

    feat: simple accordion for metrics results
    - plus the magic of * { min-width: 0 }

--- web/static/scripts/index.js
+++ web/static/scripts/index.js
@@ -157,3 +157,16 @@ function duplicateItemSection(sectionElement) {
     sectionElement.parentNode.insertBefore(sectionClone, sectionElement.parentNode.lastChild)
     document.dispatchEvent(new CustomEvent('enumerate-item-sections'))
 }
+
+function toggleAccordion(accordionContentId, iconId) {
+    const accordionContent = document.getElementById(accordionContentId)
+    accordionContent.classList.toggle('hidden')
+    accordionContent.classList.toggle('flex')
+
+    const currentIcon = document.getElementById(iconId)
+    if (accordionContent.classList.contains('flex')) {
+        currentIcon.outerHTML = `<svg id="${iconId}" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>`
+    } else {
+        currentIcon.outerHTML = `<svg id="${iconId}" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>`
+    }
+}
