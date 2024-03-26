872d39f38f2649aa2450d7a8e8e40c5c1bc84604
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-03-26 20:10:12 -0300

    feat: better setup for tests

---
+++ ss-api/tests/issue_invoice.py
@@ -0,0 +1,7 @@
+import handlers
+from . import helpers
+
+
+def e2e_test(invoice_sample_id: str):
+    invoice = helpers.get_sample(invoice_sample_id, "invoices")
+    helpers.perform_e2e_test(handlers.request_invoice_handler)(invoice)
