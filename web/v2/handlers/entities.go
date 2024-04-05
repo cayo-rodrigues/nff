d1eb39e0bf1b4f1e9ed176ad6f1fdddcb5c1b6de
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-04-05 20:10:52 -0300

    feat: entities list layout

--- web/v2/handlers/entities.go
+++ web/v2/handlers/entities.go
@@ -3,9 +3,17 @@ package handlers
 import (
 	"github.com/cayo-rodrigues/nff/web/components/layouts"
 	"github.com/cayo-rodrigues/nff/web/components/pages"
+	"github.com/cayo-rodrigues/nff/web/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func EntitiesPage(c *fiber.Ctx) error {
-	return Render(c, layouts.Base(pages.EntitiesPage()))
+	entities := []*models.Entity{
+		{Name: "Kira", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
+		{Name: "Limão", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
+		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
+		{Name: "Cay", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
+		{Name: "Gui", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
+	}
+	return Render(c, layouts.Base(pages.EntitiesPage(entities)))
 }
