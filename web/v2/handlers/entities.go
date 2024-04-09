f3733c36e10eb8e83b0badb51e6d135c951b881e
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-04-09 09:46:50 -0300

    feat: entity details

--- web/v2/handlers/entities.go
+++ web/v2/handlers/entities.go
@@ -11,19 +11,21 @@ import (
 
 func EntitiesPage(c *fiber.Ctx) error {
 	entities := []*models.Entity{
-		{Name: "Kira", CpfCnpj: "139.503.176-27", Ie: "99999990088712", UserType: "Produtor Rural"},
-		{Name: "Limão", CpfCnpj: "44.504.044/0001-24", Ie: "99999990088712", UserType: "Produtor Rural"},
-		{Name: "Ivy", CpfCnpj: "44504044000124", Ie: "024.263.939/8624", UserType: "Apenas Destinatário"},
-		{Name: "Ivy", CpfCnpj: "13950317627", Ie: "6222304167764", UserType: "Apenas Destinatário"},
-		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Emerson Cássio da Silva", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Antonio Francisco Reginaldo", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Benedito Eugenio da Fonseca Junior e outro(s)", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário"},
-		{Name: "Cay", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
-		{Name: "Gui", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural"},
+		{Name: "Kira", CpfCnpj: "139.503.176-27", Ie: "99999990088712", UserType: "Produtor Rural", Address: &models.Address{
+			StreetName: "Francisco almeida júnior ferraz",
+		}},
+		{Name: "Limão", CpfCnpj: "44.504.044/0001-24", Ie: "99999990088712", UserType: "Produtor Rural", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "44504044000124", Ie: "024.263.939/8624", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "13950317627", Ie: "6222304167764", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Emerson Cássio da Silva", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Antonio Francisco Reginaldo", ID: 2, CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Benedito Eugenio da Fonseca Junior e outro(s)", ID: 4, CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Ivy", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Apenas Destinatário", Address: &models.Address{}},
+		{Name: "Cay", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural", Address: &models.Address{}},
+		{Name: "Gui", CpfCnpj: "123123123", Ie: "99999990088712", UserType: "Produtor Rural", Address: &models.Address{}},
 	}
 	return Render(c, layouts.Base(pages.EntitiesPage(entities)))
 }
--- web/v2/tailwind.config.js
+++ web/v2/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./static/*.js",
-    "./static/*.css",
-    "./components/*.templ",
-    "./components/*/*.templ",
+    "./static/**.js",
+    "./static/**.css",
+    "./components/**.templ",
+    "./components/forms/**.templ",
+    "./components/pages/**.templ",
+    "./components/layouts/**.templ",
+    "./components/scripts/**.templ",
+    "./components/shared/**.templ",
   ],
   theme: {
     extend: {},
